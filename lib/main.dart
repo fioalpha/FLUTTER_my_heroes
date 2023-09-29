a5fa82233f7cb243a0ed5e874757c40e8cefeb74
fioalpha <fioalpha@gmail.com>
2023-09-29 13:32:48 -0300

    refactor(modular): add library to modular the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_heroes/presentation/hero_manager_widget.dart';
-import 'package:my_heroes/widgets/hero_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    )
+  );
 }
+
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    super.binds(i);
+  }
+
+  @override
+  void routes(RouteManager r) {
+    r.child('/', child: (_) => const  MyApp());
+  }
+
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: "My hero App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routerConfig: Modular.routerConfig,
+    );
+  }
+}
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            primary: Colors.blue
-        ),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My Heroes'),
